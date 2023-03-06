4b550eb11cc774f4f73f005b16a378728e5a0af6
David <lunddave@umich.edu>
2023-03-06 09:32:52 -0600

    R package in development

--- logistic_convex_hull_unadjusted.R
+++ logistic_convex_hull_unadjusted.R
@@ -2,9 +2,11 @@
 
 # https://stackoverflow.com/questions/52124359/anova-on-r-with-different-dependent-variables
 
+library(dplyr)
+
 options(digits = 6)
 
-load("/home/david/Desktop/synthetic_vol_forecasting/simulation_results/simcount_10_savetime_Mon Jan 09 00:05:44 2023.Rdata")
+load("/home/david/Desktop/synthetic_vol_forecasting/simulation_results/simcount_2_savetime_SunMar0519:17:582023_runtime_-3.11301105876764_grid_size5184_recovery_0.803144290123457_permute_0.Rdata")
 
 library(lmboot)
 
@@ -26,7 +28,8 @@ columns_we_want <- names(unique_count_df)[unique_count_df != 1]
 reduced_df <- output[names(output) %in% columns_we_want]
 
 #First specify which vol model one want to focus on
-vol_model_chosen <- reduced_df[reduced_df$vol_model == 2,]
+#vol_model_chosen <- reduced_df[reduced_df$vol_model == 1,]
+vol_model_chosen <- reduced_df
 
 #Look for missing values
 library(Amelia)
@@ -38,15 +41,29 @@ vol_model_chosen <- as.data.frame(lapply(vol_model_chosen, as.numeric))
 #Make vol model into factor
 #reduced_df$vol_model <- as.factor(reduced_df$vol_model)
 
-df_only_one_outcome <- cbind(vol_model_chosen[,c(1:10)], as.integer(vol_model_chosen$QL_adj1 < vol_model_chosen$QL_adj14))
+df_only_one_outcome <- cbind(vol_model_chosen[,c(1:11)], as.integer(vol_model_chosen$QL_adj1 <= vol_model_chosen$QL_adj14))
 
 non_NA <- df_only_one_outcome[complete.cases(df_only_one_outcome),]
 
-model <- glm(non_NA[,11] ~. , family=binomial(link='logit'),data=non_NA[,c(1:10)])
+model <- glm(non_NA[,12] ~. , family=binomial(link='logit'),data=non_NA[,c(1:6,11)])
+summary(model)
+
+model <- glm(non_NA[,12] ~.^2 , family=binomial(link='logit'),data=non_NA[,c(1:6,11)])
 summary(model)
 
 #Let's do an analysis by volatility model
 library(dplyr)
+
+## Hyperplanes
+c1 <- non_NA$n > 5
+c2 <- non_NA$p > 5
+c3 <- non_NA$arch_param < .2
+c4 <- non_NA$garch_param < .2
+c5 <- non_NA$vol_shock_length > 1
+c6 <- non_NA$vol_sig_noise_ratio > 5.9
+dom_subset <- non_NA[c1 & c2 & c3 & c4 & c5 & c6,]
+nrow(dom_subset)
+mean(dom_subset$`as.integer(vol_model_chosen$QL_adj1 <= vol_model_chosen$QL_adj14)`)
 
 means <- non_NA %>% group_by(vol_model) %>% summarise(across(everything(), list(means)))
 means <- as.data.frame(means)
