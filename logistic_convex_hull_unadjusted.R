d8d84efd91ef292196e86856d6b55064f9183cf2
David <lunddave@umich.edu>
2023-03-19 17:44:07 -0500

    alterations to proposition

--- logistic_convex_hull_unadjusted.R
+++ logistic_convex_hull_unadjusted.R
@@ -6,7 +6,7 @@ library(dplyr)
 
 options(digits = 6)
 
-load("/home/david/Desktop/synthetic_vol_forecasting/simulation_results/simcount_2_savetime_SunMar0519:17:582023_runtime_-3.11301105876764_grid_size5184_recovery_0.803144290123457_permute_0.Rdata")
+load("/home/david/Desktop/synthetic_vol_forecasting/simulation_results/simcount_2_savetime_TueMar1420:54:122023_runtime_0.389301863577631_grid_size2592_recovery_0.667631172839506_permute_0.Rdata")
 
 library(lmboot)
 
@@ -38,6 +38,9 @@ missmap(reduced_df, main = "Missing values vs observed")
 # Make the dataframe numeric
 vol_model_chosen <- as.data.frame(lapply(vol_model_chosen, as.numeric))
 
+# Now remove rows with output$M21_M22_vol_mu_delta absurdly high
+vol_model_chosen <- vol_model_chosen[vol_model_chosen$M21_M22_vol_mu_delta != .5, ]
+
 #Make vol model into factor
 #reduced_df$vol_model <- as.factor(reduced_df$vol_model)
 
@@ -45,10 +48,10 @@ df_only_one_outcome <- cbind(vol_model_chosen[,c(1:11)], as.integer(vol_model_ch
 
 non_NA <- df_only_one_outcome[complete.cases(df_only_one_outcome),]
 
-model <- glm(non_NA[,12] ~. , family=binomial(link='logit'),data=non_NA[,c(1:6,11)])
+model <- glm(non_NA[,12] ~. , family=binomial(link='logit'),data=non_NA[,c(1:5,10)])
 summary(model)
 
-model <- glm(non_NA[,12] ~.^2 , family=binomial(link='logit'),data=non_NA[,c(1:6,11)])
+model <- glm(non_NA[,12] ~.^2 , family=binomial(link='logit'),data=non_NA[,c(1:5,10)])
 summary(model)
 
 #Let's do an analysis by volatility model
@@ -57,10 +60,10 @@ library(dplyr)
 ## Hyperplanes
 c1 <- non_NA$n > 5
 c2 <- non_NA$p > 5
-c3 <- non_NA$arch_param < .2
-c4 <- non_NA$garch_param < .2
+c3 <- non_NA$arch_param < .3
+c4 <- non_NA$garch_param < .3
 c5 <- non_NA$vol_shock_length > 1
-c6 <- non_NA$vol_sig_noise_ratio > 5.9
+c6 <- non_NA$vol_sig_noise_ratio > 4.9
 dom_subset <- non_NA[c1 & c2 & c3 & c4 & c5 & c6,]
 nrow(dom_subset)
 mean(dom_subset$`as.integer(vol_model_chosen$QL_adj1 <= vol_model_chosen$QL_adj14)`)
