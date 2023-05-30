54e90829316cd4e3538b73fe3cf47c4c27a8348e
David <lunddave@umich.edu>
2023-05-29 19:03:03 -0500

    2016 example added

---
+++ R_package_development/SynthVolForecast/R/SVF_2016_election_example.R
@@ -0,0 +1,153 @@
+source('SynthVolForecast_functions.R',
+       echo = FALSE,
+       verbose = FALSE)
+
+options(digits = 7, scipen = 7)
+
+ground_truth <- c(0.000712, 0.000976)
+#ground_truth <- (0.000712)
+
+
+
+### BEGIN 2016 election example
+
+library(quantmod)
+library(bizdays)
+library(lubridate)
+
+## BEGIN USER DATA INPUTS##
+log_ret_covariates <- c("IYG" #first should be time series under study
+                        #,"GBP=X"
+                        ,"CL=F"
+                        ,"^VIX"
+                        ,"^IRX"
+                        ,"^FVX"
+                        ,"^TNX"
+                       # ,"^TYX"
+                        )
+
+level_covariates <- c(#'^VIX'
+                      #,"GBP=X"
+                      #,'^IRX'
+                      )
+
+volume_covariates <- c()
+
+shock_dates <- c("2016-11-08",
+                   "2016-06-23"
+                 , "2014-11-04"
+                 , "2012-11-06"
+                 , "2010-11-02"
+                 #, "2008-11-04"
+                 #, "2006-11-07"
+                 #, "2004-11-02"
+                 )
+
+k <- 2
+## END USER DATA INPUTS##
+
+nyse <- timeDate::holidayNYSE(2000:year(Sys.Date())+1)
+create.calendar(name='NYSE', holidays=nyse, weekdays=c('saturday', 'sunday'))
+shock_dates_as_dates <- as.Date(shock_dates)
+start_dates <- offset(shock_dates_as_dates, round(-3.5*252), "NYSE")
+k_periods_after_shock <- offset(shock_dates_as_dates, k, "NYSE")
+
+market_data_list <- vector("list", length(shock_dates))
+names(market_data_list) <- shock_dates
+
+for (i in 1:length(shock_dates)){
+
+  to_add <- lapply(log_ret_covariates, function(sym) {
+    dailyReturn(na.omit(getSymbols(sym
+                                   ,from=start_dates[i]
+                                   ,to=k_periods_after_shock[i]+10 #tk +10
+                                   ,auto.assign=FALSE))
+                                   ,type='log')})
+
+  to_add_2 <- lapply(level_covariates, function(sym) {
+    na.omit(getSymbols(sym
+                       ,from=start_dates[i]
+                       ,to=k_periods_after_shock[i]+10 #tk +10
+                       ,auto.assign=FALSE))[,6]})
+
+  to_add_3 <- lapply(volume_covariates, function(sym) {
+    na.omit(getSymbols(sym
+                       ,from=start_dates[i]
+                       ,to=k_periods_after_shock[i]+10 #tk +10
+                       ,auto.assign=FALSE))[,6]})
+
+  to_add <- c(to_add, to_add_2, to_add_3)
+  market_data_list[[i]] <- do.call(merge, to_add)
+
+}
+
+##################################
+
+#now build Y
+Y <- list()
+for (i in 1:length(start_dates)){
+  Y[[i]] <- market_data_list[[i]][,1]
+}
+
+#Now build X
+X <- list()
+for (i in 1:length(start_dates)){
+  X[[i]] <- market_data_list[[i]][,-1]
+}
+
+n <- length(start_dates) - 1
+
+#Now run the algorithm
+temp <- SynthVolForecast(Y
+                         ,X
+                         ,shock_time_vec = shock_dates
+                         ,rep(k, n+1)
+                         ,dwb_indices = NULL
+                         #,covariate_indices = length(X)
+                         ,garch_order = c(1,0,1)
+                         ,plots = TRUE
+                         ,ground_truth_vec = ground_truth)
+
+print('The point on the simplex:')
+temp$linear_combinations
+
+print('Unadjusted Prediction')
+temp$predictions$unadjusted_pred
+
+print('Adjusted Prediction')
+temp$predictions$adjusted_pred
+
+QL_loss_function <- function(pred, gt){pred/gt - log(pred/gt) - 1}
+
+QL_loss_unadjusted_pred <- mean(QL_loss_function(temp$predictions$unadjusted_pred, ground_truth))
+
+QL_loss_adjusted_pred <- mean(QL_loss_function(temp$predictions$adjusted_pred, ground_truth))
+
+dat <- c('Unadj'
+        ,'Adj'
+        ,QL_loss_unadjusted_pred
+        ,QL_loss_adjusted_pred)
+
+results_df <- as.data.frame(matrix(dat, byrow = FALSE, nrow = 2))
+colnames(results_df) <- c('Method'
+                          , 'QL_Loss')
+
+print(results_df)
+
+
+png('SVF_2016.png')
+
+dev.off()
+
+#Now run the algorithm
+#temp <- SynthPrediction(Y
+#                         ,X
+#                         ,shock_time_vec = shock_dates
+#                         ,rep(k, n+1)
+#                         ,dwb_indices = NULL
+#                         ,covariate_indices = length(X)
+#                         ,plots = TRUE
+#                        ,display_ground_truth_choice = TRUE)
+
+
+
