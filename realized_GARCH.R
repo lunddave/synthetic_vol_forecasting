332646ce7fa09f2f23b07b34f610a143db40a840
David <lunddave@umich.edu>
2023-12-22 01:35:56 -0600

    realized GARCH demo begun

--- realized_GARCH.R
+++ realized_GARCH.R
@@ -1,5 +1,3 @@
-
-
 library(rugarch)
 require(xts)
 
@@ -68,3 +66,39 @@ fc2@forecast$sigmaFor
 fc2@forecast$sigmaDF
 sigma(fc2)
 fitted(fc2)
+
+## Now using data gleaned from Wharton (WRDS) in December 2023
+wharton <- read.csv('/home/david/Desktop/synthetic_vol_forecasting/implied_vol_datasets/uw5rmndubuhtpvbt.csv')
+names(wharton)
+subset <- wharton[(wharton$days == 30) & wharton$cp_flag == 'C',]
+subset$date <- as.Date(subset$date)
+subset$impl_volatility <- as.numeric(subset$impl_volatility) * 100
+
+IYG <- read.csv('/home/david/Desktop/synthetic_vol_forecasting/implied_vol_datasets/IYG.csv')
+IYG$log_ret <- c(NA,diff(log(IYG$Adj.Close)) * 100) 
+
+merged <- merge(subset, IYG, by.x = 'date', by.y = 'Date')
+
+subset_xts <- xts(merged, order.by = merged$date)
+
+subset_xts <- subset_xts[,c('impl_volatility','log_ret')]
+
+storage.mode(subset_xts) <- "numeric"
+
+#Let's plot the realized vol column
+plot(subset_xts$impl_volatility)
+lines(subset_xts$log_ret, col = 'green')
+
+spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0), 
+                                     include.mean = FALSE), 
+                   variance.model = list(model = 'realGARCH', garchOrder = c(1, 1)))
+
+fit <- ugarchfit(spec, subset_xts$log_ret, 
+                 solver = 'hybrid', 
+                 realizedVol = subset_xts$impl_volatility)
+
+
+fit
+
+tail(sigma(fit))
+tail(fitted(fit))
