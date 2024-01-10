aa6b64e00fd22cda3dd14436fb786d4896fb80c5
David <lunddave@umich.edu>
2024-01-10 17:43:02 -0600

    Brexit removed from initial discussion of real-data example

--- realized_GARCH/step_function_in_vol.R
+++ realized_GARCH/step_function_in_vol.R
@@ -3,15 +3,20 @@ require(garchx)
 set.seed(2)
 
 # Rig the data the way we want it
-n <- 200
-step_function_series_low_vol <- rnorm(n/2, sd = .1)
-step_function_series_high_vol <- rnorm(n/2, sd = .2)
-series <- c(step_function_series_low_vol,step_function_series_high_vol)
-plot.ts(series)
+n <- 400
 
 indicator_vec_low_vol <- rep(0,n/2)
 indicator_vec_high_vol <- rep(1,n/2)
 indicator_series <- c(indicator_vec_low_vol, indicator_vec_high_vol)
+
+inn_low_vol <- rnorm(n/2, sd = .1)
+inn_high_vol <- rnorm(n/2, sd = .2)
+
+step_function_series_low_vol <- garchxSim(n/2, innovations = inn_low_vol)
+step_function_series_high_vol <- garchxSim(n/2, innovations = inn_high_vol)
+series <- c(as.vector(step_function_series_low_vol),as.vector(step_function_series_high_vol))
+plot.ts(series)
+
 
 #Fit GARCH(1,1) models and plot the fitted sigma values
 par(mfrow = c(1,2))
@@ -27,3 +32,4 @@ mod_wo_indicator
 plot.ts(mod_wo_indicator$fitted, ylab = 'Sigma^2'
         ,main = 'No Indicator Variable Results\n in Delayed Reaction\n and Erratic Estimation')
 abline(v = n/2, col = 'red')
+
